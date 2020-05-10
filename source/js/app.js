bfc7d3c73a06cf0d7614a99ac67a21d1a4a6597a
Илья Грунов <ilya.grunov@gmail.com>
2020-05-10 23:15:45 +0300

    Разметка декоративных элементов.

--- source/js/app.js
+++ source/js/app.js
@@ -1,16 +1,16 @@
-var Menu = document.querySelector(".page-header__menu");
-var MenuToggle = document.querySelector(".page-header__toggle");
+var menu = document.querySelector(".page-header__menu");
+var menuToggle = document.querySelector(".page-header__toggle");
 
-Menu.classList.remove("page-header--nojs");
-Menu.classList.add("page-header__menu--closed");
-Menu.classList.remove("page-header__menu--opened");
+menu.classList.remove("page-header--nojs");
+menu.classList.add("page-header__menu--closed");
+menu.classList.remove("page-header__menu--opened");
 
-MenuToggle.addEventListener('click', function() {
-  if (Menu.classList.contains("page-header__menu--closed")) {
-    Menu.classList.remove("page-header__menu--closed");
-    Menu.classList.add("page-header__menu--opened");
+menuToggle.addEventListener("click", function() {
+  if (menu.classList.contains("page-header__menu--closed")) {
+    menu.classList.remove("page-header__menu--closed");
+    menu.classList.add("page-header__menu--opened");
   } else {
-    Menu.classList.add("page-header__menu--closed");
-    Menu.classList.remove("page-header__menu--opened");
+    menu.classList.add("page-header__menu--closed");
+    menu.classList.remove("page-header__menu--opened");
   }
 });
